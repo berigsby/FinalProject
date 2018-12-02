a1f106f18ebe2f3ae426482d6d03cf57475d3fe5
Ben Rigsby <ber52305@uga.edu>
2018-12-01 23:45:02 -0500

    Add quiz/desc/question adding functionality

--- app/src/main/java/edu/uga/cs4060/finalproject/TeacherAddQuizQuestions.java
+++ app/src/main/java/edu/uga/cs4060/finalproject/TeacherAddQuizQuestions.java
@@ -1,12 +1,27 @@
 package edu.uga.cs4060.finalproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 
 /**
@@ -22,6 +37,16 @@ public class TeacherAddQuizQuestions extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    String classId,teacherId,studentId,quizId;
+    EditText a1,a2,a3,a4,questionTextView;
+    Button doneButton, addMoreButton;
+
+    //Database stuff
+    DataSnapshot myDataSnapshot;
+    DatabaseReference myRef;
+
+    String TAG = "AddQuizQuestion";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,13 +83,80 @@ public class TeacherAddQuizQuestions extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        ((FloatingActionButton)((TeacherMenuElement)getActivity()).findViewById(R.id.fab)).hide();
+
+        //Accessing the firebase test
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        // Read from the database
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = "test";
+
+                myDataSnapshot = dataSnapshot;
+
+                //textView.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teacher_add_quiz_questions, container, false);
+        View aview = inflater.inflate(R.layout.fragment_teacher_add_quiz_questions, container, false);
+
+        aview.findViewById(R.id.addMoreButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                questionTextView = getActivity().findViewById(R.id.textView5);
+                a1 = getActivity().findViewById(R.id.a1);
+                a2 = getActivity().findViewById(R.id.a2);
+                a3 = getActivity().findViewById(R.id.a3);
+                a4 = getActivity().findViewById(R.id.a4);
+                RadioGroup radioGroup = getActivity().findViewById(R.id.radioGroup);
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                int ans = 0;
+                if(selectedId == R.id.answerRadio1){
+                    ans = 1;
+                } else if(selectedId == R.id.answerRadio2){
+                    ans = 2;
+                } else if(selectedId == R.id.answerRadio3){
+                    ans = 3;
+                } else if(selectedId == R.id.answerRadio4){
+                    ans = 4;
+                } else {
+                    //TODO show error...
+                }
+                QuestionPojo questionPojo = new QuestionPojo("",quizId,questionTextView.getText().toString(),
+                        a1.getText().toString(),a2.getText().toString(),a3.getText().toString(),a4.getText().toString(),ans);
+                MyFirebaseHelper.create(myRef,questionPojo);
+
+            }
+        });
+
+        aview.findViewById(R.id.doneButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                addMoreButton.callOnClick();
+                //TODO go back to the quizList
+            }
+        });
+
+        return aview;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -77,12 +169,12 @@ public class TeacherAddQuizQuestions extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        //if (context instanceof OnFragmentInteractionListener) {
+        //    mListener = (OnFragmentInteractionListener) context;
+        //} else {
+        //    throw new RuntimeException(context.toString()
+        //            + " must implement OnFragmentInteractionListener");
+        //}
     }
 
     @Override
--- app/src/main/java/edu/uga/cs4060/finalproject/TeacherAddQuizTitleDescFragment.java
+++ app/src/main/java/edu/uga/cs4060/finalproject/TeacherAddQuizTitleDescFragment.java
@@ -5,9 +5,18 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -29,6 +38,17 @@ public class TeacherAddQuizTitleDescFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    EditText editQuizTitle,editQuizDesc;
+    Button buttonCreateQuiz;
+
+    String classId,teacherId;
+
+    //Database stuff
+    DataSnapshot myDataSnapshot;
+    DatabaseReference myRef;
+
+    String TAG = "AddQuizTitleDesc";
 
     public TeacherAddQuizTitleDescFragment() {
         // Required empty public constructor
@@ -60,13 +80,54 @@ public class TeacherAddQuizTitleDescFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
         ((FloatingActionButton)((TeacherMenuElement)getActivity()).findViewById(R.id.fab)).hide();
+
+        //Accessing the firebase test
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        // Read from the database
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = "test";
+
+                myDataSnapshot = dataSnapshot;
+
+                //textView.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teacher_add_quiz_title_desc, container, false);
+        View aview = inflater.inflate(R.layout.fragment_teacher_add_quiz_title_desc, container, false);
+        aview.findViewById(R.id.buttonCreateQuiz).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                editQuizTitle = getActivity().findViewById(R.id.editQuizTitle);
+                editQuizDesc = getActivity().findViewById(R.id.editQuizDesc);
+
+                QuizPojo quizPojo = new QuizPojo("",classId,editQuizTitle.getText().toString(),editQuizDesc.getText().toString());
+                MyFirebaseHelper.create(myRef,quizPojo);
+                //TODO call addquizquestions
+
+            }
+        });
+        return aview;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
